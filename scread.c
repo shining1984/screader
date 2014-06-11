503ace0161f0f35991287060ba35f80e9af6b11c
shining1984 <shiningning1984@gmail.com>
2014-06-11 11:27:17 +0800

    Go through all the AST nodes.
        Go through all the AST nodes,
        and output the name of the AST
        nodes.

--- scread.c
+++ scread.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include "Index.h"
 
+enum CXChildVisitResult printVisitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
+
+    const char *astName = clang_getCString(clang_getCursorSpelling(cursor));
+    printf("The AST name is:%s\n", astName);
+
+    return CXChildVisit_Recurse;
+}
+
 
 int main(int argc, char *argv[]) {
-  CXIndex Index = clang_createIndex(0,0);
-  printf("The scread is working.\n");
-  clang_disposeIndex(Index);
-  return 0;
+    CXIndex Index = clang_createIndex(0,0);
+    CXTranslationUnit TU = clang_parseTranslationUnit(Index, 0, argv, argc,
+                                                      0, 0,
+                                                      CXTranslationUnit_None);
+    CXCursor C = clang_getTranslationUnitCursor(TU);
+    const char *astName = clang_getCString(clang_getCursorSpelling(C));
+    printf("The AST name is:%s\n", astName);
+
+    clang_visitChildren(C, printVisitor, NULL);
+
+    clang_disposeTranslationUnit(TU);
+    clang_disposeIndex(Index);
+    return 0;
 }
