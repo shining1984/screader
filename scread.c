917f32b968fc2f5ed4d7acd0b3916e6404566960
shining1984 <shiningning1984@gmail.com>
2014-06-11 11:29:26 +0800

    Print the start and end postion of ast node.

--- scread.c
+++ scread.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "Index.h"
+
+bool printLocation(CXCursor cursor) {
+    CXSourceRange range = clang_getCursorExtent(cursor);
+    CXSourceLocation startLocation = clang_getRangeStart(range);
+    CXSourceLocation endLocation = clang_getRangeEnd(range);
+
+    CXFile file;
+    unsigned int line, column, offset;
+    clang_getInstantiationLocation(startLocation, &file, &line, &column, &offset);
+    printf("Start: Line: %u Column: %u Offset: %u\n", line, column, offset);
+    clang_getInstantiationLocation(endLocation, &file, &line, &column, &offset);
+    printf("End: Line: %u Column: %u Offset: %u\n", line, column, offset);
+
+    return true;
+}
 
 enum CXChildVisitResult printVisitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
 
     const char *astName = clang_getCString(clang_getCursorSpelling(cursor));
     printf("The AST name is:%s\n", astName);
 
+    printLocation(cursor);
     return CXChildVisit_Recurse;
 }
-
 
 int main(int argc, char *argv[]) {
     CXIndex Index = clang_createIndex(0,0);
@@ -18,6 +34,7 @@ int main(int argc, char *argv[]) {
     CXCursor C = clang_getTranslationUnitCursor(TU);
     const char *astName = clang_getCString(clang_getCursorSpelling(C));
     printf("The AST name is:%s\n", astName);
+    printLocation(C);
 
     clang_visitChildren(C, printVisitor, NULL);
 
